1e47b6aaf5e62d3274672efd4fe569c33edb91ef
Wiktor <wiktor-wosiek@wp.pl>
2025-11-13 11:09:11 +0100

    Ale mow mi Wicia

---
+++ Models/Character.cs
@@ -0,0 +1,15 @@
+namespace lekcja7.Models;
+
+public class Character
+{
+    public string Name { get; set; } = "";
+    public string[] Movies { get; set; } = [];
+    public string Role { get; set; } = "";
+    public string Actor { get; set; } = "";
+    public string Rase { get; set; } = "";
+    public int BirthDate { get; set; } = 0;
+    public string Function { get; set; } = "";
+    public string Characteristics { get; set; } = "";
+    public string Fate { get; set; } = "";
+    public string FunFact { get; set; } = "";
+};
